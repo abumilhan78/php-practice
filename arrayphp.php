77ee85cddf8f97ba5b0b85f4e2fdfc4abfa45e4d
abumilhan78 <abumilhan78@gmail.com>
2019-07-31 16:33:24 +0700

    New Updates!!

---
+++ arrayphp.php
@@ -0,0 +1,12 @@
+<?php
+$arrBuah = array ("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrBuah[1]; //Apel
+echo $arrBuah[3]; //Jeruk
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+echo $arrWarna[4]; //Putih
+echo $arrWarna[1]; //Biru
+?>
