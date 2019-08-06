17bed93ae1628aaa094bb0ce063f212f52709de2
abumilhan78 <abumilhan78@gmail.com>
2019-08-06 22:11:02 +0700

    New Product!!

---
+++ date.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<br>". date("d/m/Y H:i:s");
+
+echo "<br>". date("F j, Y, g:i a");
+
+echo "<br>". date("d.m.y");
+
+echo "<br>". date("Ymd");
+
+echo "<br>". date('j-m-y, it is w Day z ');
+
+echo "<br>". date('\i\t \i\s \t\h\e jS \d\a\y.');
+
+echo "<br>". date("D M j G:i:s T Y");
+echo "<br>". date("H:i:s");
+
+?>
