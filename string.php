97878992b25e7a4d0654ebad76a06786db921c9c
abumilhan78 <abumilhan78@gmail.com>
2019-08-06 22:47:57 +0700

    explode added!

--- string.php
+++ string.php
@@ -26,6 +26,19 @@ echo htmlspecialchars ($string)."<br>";
 //tampil apa adanya
 echo htmlentities ($string);
 
+echo "<br><br><br>Explode!<br><br>";
+// Contoh 1
+$buah = "mangga pisang rambutan apel jeruk kedondong";
+$buahan = explode(" ", $buah);
+echo $buahan[0]."<br>"; // mangga
+echo $buahan[3]."<br>"; // apel
+// Contoh 2
+$data = "foo:*:1023:1000::/home/foo:/bin/sh";
+list($user, $pass, $uid, $gid, $gecos, $home, $shell) =
+explode(":", $data);
+echo $user."<br>"; // foo
+echo $pass; // *
+
 ?>
 
 
