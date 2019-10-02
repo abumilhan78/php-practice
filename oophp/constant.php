7ca56cc6492453ccc6f34abf7793e2ce2d53b72a
abumilhan78 <abumilhan78@gmail.com>
2019-10-02 23:15:38 +0700

    New Material

---
+++ oophp/constant.php
@@ -0,0 +1,30 @@
+<?php
+
+// define('NAMA', 'Mochammad Qaysa al-Haq');
+
+// echo NAMA;
+
+// const UMUR = 16;
+// echo "<br>".UMUR;
+
+/* 
+Contoh Magic Constant :
+__LINE__ = UNTUK MENENTUKAN DIBARIS MANA
+__FILE__ = LETAK FILE
+__DIR__ = LETAK DIR
+__FUNCTION__ = FUN YANG DIGUNAKAN
+__CLASS__ = CLASS YANG DIGUNAKAN
+__TRAIT__
+__METHOD__
+__NAMESPACES__
+
+*/
+
+class Coba {
+  const NAMA = "Qaysa";
+
+}
+
+echo Coba::NAMA;
+
+?>
